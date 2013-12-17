2481ecf42e612498453e5925b4d1d8d33d70e10d
Adam McCrea <adam@adamlogic.com>
2013-12-17 14:16:04 -0500

    Include into AR outside of mixin file so it can be tested

--- lib/dirty_history.rb
+++ lib/dirty_history.rb
@@ -6,5 +6,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 autoload :DirtyHistoryRecord, "dirty_history/dirty_history_record"
 require "dirty_history/dirty_history_mixin"  
 
+ActiveRecord::Base.send :include, DirtyHistory::Mixin
+
 $LOAD_PATH.shift
 
